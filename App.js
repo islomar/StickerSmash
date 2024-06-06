4d27a8f9fd5f8666a44a92db44b280fff32d9fed
Isidro Lopez <islomar@gmail.com>
2024-06-06 14:00:10 +0200

    Use the selected image

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { StyleSheet, View} from "react-native";
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import * as ImagePicker from "expo-image-picker";
+import {useState} from "react";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+    const [selectedImage, setSelectedImage] = useState(null);
+
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             allowsEditing: true,
@@ -15,7 +18,7 @@ export default function App() {
         });
 
         if (!result.canceled) {
-            console.log(result);
+            setSelectedImage(result.assets[0].uri);
         } else {
             alert('You did not select any image.');
         }
@@ -24,7 +27,10 @@ export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-                <ImageViewer placeholderImageSource={PlaceholderImage} />
+                <ImageViewer
+                    placeholderImageSource={PlaceholderImage}
+                    selectedImage={selectedImage}
+                />
             </View>
             <View style={styles.footerContainer}>
                 <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
