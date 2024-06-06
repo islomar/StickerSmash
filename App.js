2825b14d7849847ad5d36ae98a50e64ddc6020d7
Isidro Lopez <islomar@gmail.com>
2024-06-06 13:32:41 +0200

    Change the text color

--- App.js
+++ App.js
@@ -2,19 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={{ color: '#fff' }}>
+                Open up App.js to start working on your app!
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
