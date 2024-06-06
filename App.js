9d315f5a121d3783c8fff619ae726667d04883f2
Isidro Lopez <islomar@gmail.com>
2024-06-06 16:17:05 +0200

    Add buttons when an image has been selected

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import * as ImagePicker from "expo-image-picker";
 import {useState} from "react";
+import IconButton from "./components/IconButton";
+import CircleButton from "./components/CircleButton";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -26,6 +28,18 @@ export default function App() {
         }
     };
 
+    const onReset = () => {
+        setShowAppOptions(false);
+    };
+
+    const onAddSticker = () => {
+        // we will implement this later
+    };
+
+    const onSaveImageAsync = async () => {
+        // we will implement this later
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
@@ -35,7 +49,13 @@ export default function App() {
                 />
             </View>
             {showAppOptions ? (
-                    <View/>
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton icon="refresh" label="Reset" onPress={onReset} />
+                        <CircleButton onPress={onAddSticker} />
+                        <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+                    </View>
+                </View>
                 ) : (
                 <View style={styles.footerContainer}>
                     <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
@@ -61,5 +81,13 @@ const styles = StyleSheet.create({
     footerContainer: {
         flex: 1 / 3,
         alignItems: 'center',
+    },
+    optionsContainer: {
+        position: 'absolute',
+        bottom: 80,
+    },
+    optionsRow: {
+        alignItems: 'center',
+        flexDirection: 'row',
     },
 });
